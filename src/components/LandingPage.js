9ba42335133c83c82daac8f688673142869d94b6
Akhilesh Thite <akhithite@gmail.com>
2023-03-31 00:01:25 +0530

    improve responsiveness of header

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -32,7 +32,7 @@ function LandingPage() {
     <div style={{ backgroundColor: "#FFFCF9" }}>
       {/* header */}
       <section className="text-gray-600 body-font">
-        <div className="container mx-auto flex px-5 sm:px-10 md:px-20 lg:px-32 xl:px-38 py-24 md:flex-row flex-col items-center">
+        <div className="container mx-auto flex px-5 sm:px-10 md:px-20 lg:px-32 xl:px-38 py-16 sm:py-24 md:flex-row flex-col items-center">
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
               <br className="hidden lg:inline-block" />
